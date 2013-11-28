c9aacf2acd97c60be08f1a59e20bf998e0c646bd
Chris Lyle <chrislyle@blueyonder.co.uk>
2013-11-28 23:01:47 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KanteenApp::Application.config.secret_key_base = '0b3166a012630a453792771212a43efc7e74dfbe2a7f28d92f87ceea4b9b49e16b4e3f7088fe17d1fed46264bd5adb3e992405cea1729d4d03b72f9cbdb54e95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KanteenApp::Application.config.session_store :cookie_store, key: '_kanteen_app_session'
